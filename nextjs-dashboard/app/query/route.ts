9482e93a95285386d7aab8fa74511e65fdb568ac
RadostinTasev1999 <radostintasev22@yahoo.com>
2026-08-12 17:38:05 +0300

    Added functionality for data fetching for dashboard overview page

--- nextjs-dashboard/app/query/route.ts
+++ nextjs-dashboard/app/query/route.ts
@@ -1,26 +1,23 @@
-// import postgres from 'postgres';
+ import postgres from 'postgres';
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' }); // An utility function to make queries to the server
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
+async function listInvoices() {
 
-// 	return data;
-// }
+	const data = await sql`
+    SELECT invoices.amount, customers.name
+    FROM invoices
+    JOIN customers ON invoices.customer_id = customers.id
+    WHERE invoices.amount = 54246;
+  `;
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+	return data;
+}
+
+export async function GET() { 
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
